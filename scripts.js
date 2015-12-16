89f9b0bbcdc5d7ef1211362f989c5bd27b749511
SecondHong <whpigpig@gmail.com>
2015-12-17 01:45:56 +0800

    Update Docs and styles.

--- scripts.js
+++ scripts.js
@@ -8,4 +8,22 @@ $(document).ready(function(e) {
         debug: true
     });
 
+    var generalAjaxHandler = {
+        successEnd: function (data, textStatus, jqXHR) {
+            console.log('!!!!!!Rediect!!!!!!');
+        }
+    };
+
+    var injectAjaxHandler = {
+        alertUser: function (data, textStatus, jqXHR) {
+            alert('This is this injectAjaxHandler.');
+        }
+    };
+
+    $('.ajax-request').ajaxFormEx({
+        debug: true,
+        ajaxHandler: generalAjaxHandler,
+        injectAjaxHandler: injectAjaxHandler
+    });
+
 });
