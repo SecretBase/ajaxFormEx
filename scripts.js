5404c1cba3e2fe54d195a80a605aea16da7a0491
SecondHong <whpigpig@gmail.com>
2015-12-17 23:21:38 +0800

    Update Docs

--- scripts.js
+++ scripts.js
@@ -9,7 +9,13 @@ $(document).ready(function(e) {
     });
 
     var generalAjaxHandler = {
+        beforesend: function (jqXHR, settings) {
+
+
+
+        },
         successEnd: function (data, textStatus, jqXHR) {
+            console.log(data);
             console.log('!!!!!!Rediect!!!!!!');
         }
     };
@@ -19,6 +25,27 @@ $(document).ready(function(e) {
             alert('This is this injectAjaxHandler.');
         }
     };
+
+    $('.change-url').on('click', function (event) {
+
+        event.preventDefault();
+
+        var $this = $(event.currentTarget);
+
+        injectAjaxHandler.changeUrl = function (jqXHR, settings) {
+
+            console.log(this.attr('data-form-beforesend'));
+
+            settings.url = event.currentTarget.getAttribute('data-url');
+
+            this.removeAttr('data-form-beforesend');
+
+        };
+
+        $this.closest('form').attr('data-form-beforesend', 'changeUrl').trigger('submit');
+
+    });
+
 
     $('.ajax-request').ajaxFormEx({
         debug: true,
